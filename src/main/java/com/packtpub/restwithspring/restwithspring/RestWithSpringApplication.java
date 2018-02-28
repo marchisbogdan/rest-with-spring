665f01c0b1e7c924e7cb2f05c4c245898f8de31a
Bogdan Marchis <bogdan.marchis@cleverhouse-engineering.com>
2018-02-28 14:44:38 +0200

    Source files added

---
+++ src/main/java/com/packtpub/restwithspring/restwithspring/RestWithSpringApplication.java
@@ -0,0 +1,12 @@
+package com.packtpub.restwithspring.restwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringApplication.class, args);
+	}
+}
